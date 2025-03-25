ef266b62f62d5bbec71cf8ba671b17db70670db6
Kleber <kleber@user>
2025-03-25 12:43:45 -0300

    all procedures added

--- 04.Procedure/procedures.sql
+++ 04.Procedure/procedures.sql
@@ -1,4 +1,5 @@
 -- Procedures
+use Biblioteca;
 
 -- Insert
 delimiter $
@@ -7,7 +8,7 @@ CPF int(11), telefone int(10), genero enum('F','M'))
 begin
 insert clientes values
 (id_c,id_m,id_l,nome,CPF,telefone,genero),
-end &
+end $
 delimiter ;
 
 delimiter $
@@ -16,7 +17,7 @@ no_p int, ISBN int)
 begin
 insert livros values
 (id_l,id_c,titulo,no_p,ISBN)
-end &
+end $
 delimiter ;
 
 delimiter $
@@ -24,42 +25,64 @@ create procedure as Adicionar_Categoria(id_c int, tipo varchar(200))
 begin
 insert categorias values
 (id_c,tipo)
-end &
+end $
 delimiter ;
 
 delimiter $
 create procedure as Adicionar_Emprestimo(id_emprestimo int, id_cliente int not null,
 id_livro int)
 begin
-insert categorias values
+insert emprestimo values
 (id_emprestimo, id_cliente, id_livro, data_emprestimo, data_revolucao)
-end &
+end $
 delimiter ;
 
 delimiter $
 create procedure as Adicionar_Multa(id_multa int, id_emprestimo int, id_cliente int, valor int, tipo varchar(200))
 begin
-insert categorias values
+insert multa values
 (id_multa, id_emprestimo, id_cliente, valor, tipo)
-end &
+end $
 delimiter ;
 
 delimiter $
-create procedure as Adicionar_categorias(id_categoria int, tipo varchar(200))
+create procedure as Adicionar_autores(id_autor int, editora varchar(200) , nome varchar(200))
 begin
-insert categorias values
-(id_categoria, tipo)
-end &
+insert autores values
+(id_autor, editora, nome)
+end $
 delimiter ;
 
 delimiter $
-create procedure as Adicionar_autores(id_categoria int, tipo varchar(200))
+create procedure as Adicionar_livro_categoria(
+id_categoria_livro int,
+id_livro int,
+id_categoria int)
 begin
-insert categorias values
-(id_categoria, tipo)
-end &
+insert livro_categoria values
+(id_categoria_livro, id_livro, id_categoria)
+end $
+delimiter ;
+
+delimiter $
+create procedure as Adicionar_livro_autor(
+id_livro_cliente int, id_livro int, id_categoria int)
+begin
+insert livro_autor VALUES
+(id_categoria_livro, id_livro, id_categoria)
+end $
 delimiter ;
 
 -- Update
+delimiter $
+create procedure as Update_autores(id_autor int, editora varchar(200) , nome varchar(200))
+BEGIN
+UPDATE autores
+set editora = editora, nome = nome
+where id_autor = id_autor
+set 
+delimiter ;
+
+
 
 -- Delete
